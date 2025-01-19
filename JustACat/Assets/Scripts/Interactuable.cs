7530beec0fd8231c2cf31b0118d3f77eb6a8171d
Ernesto Reyes Alvarez <ernesto.reyes.alvarez@gmail.com>
2025-01-19 14:07:58 -0800

    Modificaciones

--- JustACat/Assets/Scripts/Interactuable.cs
+++ JustACat/Assets/Scripts/Interactuable.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interactuable : MonoBehaviour
 {
-   
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext ctx)
+    { 
+        if(!ctx.started)
+            return;
+
+        var rayHit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+
+        if(!rayHit.collider)
+            return;
+
+        Debug.Log(rayHit.collider.gameObject.name);
+    }
 }
